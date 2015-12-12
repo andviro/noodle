c0e87513cbfb50d0553cf9e066c50e0c6eb17604
Andrew Rodionoff <andviro@gmail.com>
2015-12-13 00:58:09 +0300

    minor optimization: strip prefix only in groups

--- wok.go
+++ wok.go
@@ -21,15 +21,13 @@ type Wok struct {
 var methods = []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}
 var paramKey key = 0
 
-func restorePrefix(prefix string) noodle.Middleware {
-	return func(next noodle.Handler) noodle.Handler {
-		if prefix == "" {
-			return next
-		}
-		return func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-			r.URL.Path = prefix + r.URL.Path
-			return next(ctx, w, r)
-		}
+func (wok *Wok) restorePrefix(next noodle.Handler) noodle.Handler {
+	if wok.prefix == "" {
+		return next
+	}
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		r.URL.Path = wok.prefix + r.URL.Path
+		return next(ctx, w, r)
 	}
 }
 
@@ -38,12 +36,6 @@ func (wok *Wok) convert(h noodle.Handler) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		_ = h(context.WithValue(wok.rootCtx, paramKey, p), w, r)
 	}
-}
-
-// ServeHTTP allows Wok to implement http.Handler interface
-func (wok *Wok) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Prefix needs to be stripped do maintain composeability
-	http.StripPrefix(wok.prefix, wok.Router).ServeHTTP(w, r)
 }
 
 // New creates new Wok initialized with middlewares.
@@ -69,16 +61,16 @@ func (wok *Wok) Group(prefix string, mws ...noodle.Middleware) *Wok {
 	}
 
 	res := &Wok{
-		prefix: prefix,
-		Router: httprouter.New(),
-		// prefix needs to be stuffed back into request path to fool middlewares
-		chain:   noodle.New(restorePrefix(prefix)).Use(wok.chain...).Use(mws...),
+		prefix:  prefix,
+		Router:  httprouter.New(),
 		rootCtx: wok.rootCtx,
 	}
+	// prefix needs to be stuffed back into request path to fool middlewares
+	res.chain = noodle.New(res.restorePrefix).Use(wok.chain...).Use(mws...)
 
 	res.prefix = prefix
 	for _, m := range methods {
-		wok.Handler(m, prefix+"/*rest", res)
+		wok.Handler(m, prefix+"/*rest", http.StripPrefix(res.prefix, res))
 	}
 	return res
 }
