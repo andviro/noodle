0a408766abc578154e5d80a33f1912efce9e86b3
Andrew Rodionoff <andviro@gmail.com>
2016-05-21 19:22:54 +0300

    uh-oh, realIP should really be before logger in chain

--- middleware/default.go
+++ middleware/default.go
@@ -6,5 +6,5 @@ import (
 
 // Default is a convenience function creating new noodle.Chain with Logger, Recover and LocalStore middlewares
 func Default(mws ...noodle.Middleware) noodle.Chain {
-	return noodle.New(Logger, Recover, RealIP, LocalStore).Use(mws...)
+	return noodle.New(RealIP, Logger, Recover, LocalStore).Use(mws...)
 }
