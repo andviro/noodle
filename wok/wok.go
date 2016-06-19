f8c46dda1ad4530ea8dc1c3d140bebf39715c50b
Andrew Rodionoff <andviro@gmail.com>
2016-06-20 02:10:01 +0300

    added method to set initial context for route groups

--- wok/wok.go
+++ wok/wok.go
@@ -21,12 +21,16 @@ type RouteClosure func(noodle.Handler)
 
 type key int
 
-var paramKey key = 0
+const (
+	paramKey key = iota
+)
+
+var todoCtx = context.TODO()
 
 // convert turns noodle.Handler into httprouter.Handle
 func (wok *Wok) convert(h noodle.Handler) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		_ = h(context.WithValue(wok.rootCtx, paramKey, p), w, r)
+		_ = h(context.WithValue(wok.context(), paramKey, p), w, r)
 	}
 }
 
@@ -34,33 +38,50 @@ func (wok *Wok) convert(h noodle.Handler) httprouter.Handle {
 // The resulting middleware chain will be called for all routes in Wok
 func New(mws ...noodle.Middleware) *Wok {
 	return &Wok{
-		Router:  httprouter.New(),
-		chain:   noodle.New(mws...),
-		rootCtx: context.TODO(),
+		Router: httprouter.New(),
+		chain:  noodle.New(mws...),
 	}
+}
+
+// context determines context for the handler.
+func (wok *Wok) context() context.Context {
+	if wok.rootCtx != nil {
+		return wok.rootCtx
+	}
+	if wok.parent != nil {
+		return wok.parent.context()
+	}
+	return todoCtx
+}
+
+// SetRootCtx injects user-supplied context into the router.
+// Note that you can set the context for subrouters.
+// If subrouter context is not set explicitly, it will be inherited from its parent.
+func (wok *Wok) SetContext(ctx context.Context) {
+	wok.rootCtx = ctx
 }
 
 // Handle allows to attach some noodle Middlewares and a Handle to a route
 func (wok *Wok) Handle(method, path string, mws ...noodle.Middleware) RouteClosure {
-	chain := wok.chain.Use(mws...)
-	if wok.parent == nil {
-		return func(h noodle.Handler) {
-			h = chain.Then(h)
-			wok.Router.Handle(method, path, wok.convert(h))
-		}
+	chain := noodle.New(mws...)
+	for router := wok; router != nil; router = router.parent {
+		chain = router.chain.Use(chain...)
+		path = filepath.Join(router.prefix, path)
 	}
-	return wok.parent.Handle(method, filepath.Join(wok.prefix, path), chain...)
+	return func(h noodle.Handler) {
+		h = chain.Then(h)
+		wok.Router.Handle(method, path, wok.convert(h))
+	}
 }
 
 // Group starts new route group with common prefix.
 // Middleware passed to Group will be used for all routes in it.
 func (wok *Wok) Group(prefix string, mws ...noodle.Middleware) *Wok {
 	return &Wok{
-		prefix:  prefix,
-		parent:  wok,
-		Router:  wok.Router,
-		chain:   noodle.New(mws...),
-		rootCtx: wok.rootCtx,
+		prefix: prefix,
+		parent: wok,
+		Router: wok.Router,
+		chain:  noodle.New(mws...),
 	}
 }
 
